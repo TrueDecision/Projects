91d25834e5867fce869f0bed0e79ac7cc209deea
TrueDecision <57047467+TrueDecision@users.noreply.github.com>
2019-11-27 18:04:28 +0600

    Add files via upload

---
+++ Sity/src/js/dropdown.js
@@ -0,0 +1,20 @@
+btn.onclick = function(){
+ menu.classList.toggle('open');
+}
+
+map_btn.onclick = function(){
+  window.scrollBy(0, -2200)
+  map.classList.toggle('open');
+  cat.classList.toggle('none');
+}
+
+map_btn1.onclick = function(){
+  map.classList.toggle('open');
+  cat.classList.toggle('none');
+}
+
+
+
+dropdown1.onclick = function(){
+  dd_cell_1.classList.toggle('open');
+}
